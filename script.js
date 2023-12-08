13879f7110ea55fbc17c964f0ea6677c9cc8840f
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-12-08 20:35:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll(".code");
 
-const inputs = document.querySelectorAll('.code');
+inputs.forEach((input,index) => {
+	input.addEventListener('input', (event) => {
+	const inputLength = event.target.value.length;
+		if(inputLength === 1 && index < inputs.length - 1){
+			inputs[index + 1].focus();
+		}	
+	});
 
-// Add event listeners to inputs
-inputs.forEach((input, index) => {
-  input.addEventListener('input', (event) => {
-    const currentValue = event.target.value;
-    if (currentValue.length === 1 && index < inputs.length - 1) {
-      inputs[index + 1].focus();
-    }
-  });
-
-  input.addEventListener('keydown', (event) => {
-    if (event.key === 'Backspace' && index > 0) {
-      if (inputs[index].value === '') {
-        inputs[index - 1].focus();
-      }
-    }
-  });
+	input.addEventListener('keydown', (event) => {
+		if(event.key === 'Backspace' && index > 0){
+			if(inputs[index].value === ''){
+				inputs[index - 1].focus();
+			}
+		}
+	});
+	
 });
